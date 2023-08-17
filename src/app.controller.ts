bb2cc8abde319aed7e947f522ed9499fad422e91
linh2206 <lehoailinh2206@gmail.com>
2023-08-17 17:37:18 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query() query: any) {
+    return this.appService.getHello(query);
+  }
+  @Post()
+  create(@Req() request: Request, @Query() query: any){
+    return this.appService.create(request.body, query)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs/promises';
+var fs = require('fs');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  create(params, query) {
+    console.log("🚀 ~ file: app.service.ts:8 ~ AppService ~ create ~ query:", query)
+    fs.writeFile(query.name + ".json", JSON.stringify(params), function (err) {
+      if (err) throw err;
+      console.log('complete');
+    }
+    );
+    return "complete";
+  }
+  async getHello(query) {
+    let data = JSON.parse(await readFile(query.name + ".json", "utf8"));
+    return data;
   }
 }
